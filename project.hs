77053e944ef8b4cf92b3c1c0e4c3f9ef0c7ceb7f
Domagoj Krivosic <domagoj@Domagojs-MacBook-Pro.local>
2017-01-15 18:10:44 +0100

    Fix bug in substitution, add simplification to substitution and derivation

--- project.hs
+++ project.hs
@@ -3,90 +3,28 @@ import Data.Char
 type Op = (String, Double -> Double -> Double)
 data Node = Operator Op Node Node | Function String Node | Variable String | Constant Double
 
-instance Show Node where
-  show (Constant x)                                            = show x
-  show (Variable x)                                            = x
-  show (Function f x)                                          = f ++ " ( " ++ show x ++ " ) "
-  show (Operator ("*", _) x@(Operator (op1, _) _ _) y@(Operator (op2, _) _ _))
-    | (op1 == "+" || op1 == "-") && (op2 == "+" || op2 == "-") = " ( " ++ show x ++ " ) * ( " ++ show y ++ " ) "
-    | op1 == "+" || op1 == "-"                                 = " ( " ++ show x ++ " ) * " ++ show y
-    | op2 == "+" || op2 == "-"                                 = show x ++ " * ( " ++ show y ++ " ) "
-    | otherwise                                                = show x ++ " * " ++ show y
-  show (Operator ("*", _) x y@(Operator (op, _) _ _))
-    | op == "+" || op == "-"                                   = show x ++ " * ( " ++ show y ++ " ) "
-    | otherwise                                                = show x ++ " * " ++ show y
-  show (Operator ("*", _) x@(Operator (op, _) _ _) y)
-    | op == "+" || op == "-"                                   = "( " ++ show x ++ " ) * " ++ show y
-    | otherwise                                                = show x ++ " * " ++ show y
-  show (Operator ("/", _) x@(Operator (op1, _) _ _) y@(Operator (op2, _) _ _))
-    | (op1 == "+" || op1 == "-") && (op2 == "+" || op2 == "-") = " ( " ++ show x ++ " ) / ( " ++ show y ++ " ) "
-    | op1 == "+" || op1 == "-"                                 = " ( " ++ show x ++ " ) / " ++ show y
-    | op2 == "+" || op2 == "-"                                 = show x ++ " / ( " ++ show y ++ " ) "
-    | otherwise                                                = show x ++ " / " ++ show y
-  show (Operator ("/", _) x y@(Operator (op, _) _ _))
-    | op == "+" || op == "-"                                   = show x ++ " / ( " ++ show y ++ " ) "
-    | otherwise                                                = show x ++ " / " ++ show y
-  show (Operator ("/", _) x@(Operator (op, _) _ _) y)
-    | op == "+" || op == "-"                                   = "( " ++ show x ++ " ) / " ++ show y
-    | otherwise                                                = show x ++ " / " ++ show y
-  show (Operator ("^", _) x@(Operator (op, _) _ _) y)
-    | op == "+" || op == "-"                                   = " ( " ++ show x ++ " ) ^ " ++ show y
-    | otherwise                                                = show x ++ " " ++ op ++ " " ++ show y
-  show (Operator op x y)                                       = show x ++ " " ++ fst op ++ " " ++ show y
-
-instance Eq Node where
-  (Constant x) == (Constant y)                     = x == y
-  (Variable x) == (Variable y)                     = x == y
-  (Function f x) == (Function g y)                 = f == g && x == y
-  (Operator (o1, _) a b) == (Operator (o2, _) c d) = o1 == o2 && a == c && b == d
-  _ == _                                           = False
-
-simplify :: Node -> Node
-simplify t = if t == tt then t else simplify tt
-  where tt = simplify' t
-
-simplify' :: Node -> Node
-simplify' (Constant x)                          = Constant x
-simplify' (Variable x)                          = Variable x
-simplify' (Function f x)                        = Function f (simplify' x)
-simplify' (Operator ("*", (*)) (Constant 0) x)  = Constant 0
-simplify' (Operator ("*", (*)) x (Constant 0))  = Constant 0
-simplify' (Operator ("*", (*)) x (Constant 1))  = simplify' x
-simplify' (Operator ("*", (*)) (Constant 1) x)  = simplify' x
-simplify' (Operator ("*", (*)) x y)             = Operator ("*", (*)) (simplify' x) (simplify' y)
-simplify' (Operator ("^", (**)) x (Constant 1)) = simplify' x
-simplify' (Operator ("^", (**)) x (Constant 0)) = Constant 1
-simplify' (Operator ("^", (**)) (Constant 1) x) = Constant 1
-simplify' (Operator ("^", (**)) x y)            = Operator ("^", (**)) (simplify' x) (simplify' y)
-simplify' (Operator ("/", (/)) (Constant 0) y)  = Constant 0
-simplify' (Operator ("/", (/)) x (Constant 1))  = simplify' x
-simplify' (Operator ("/", (/)) x y)             = Operator ("/", (/)) (simplify' x) (simplify' y)
-simplify' (Operator ("+", (+)) (Constant 0) y)  = simplify' y
-simplify' (Operator ("+", (+)) x (Constant 0))  = simplify' x
-simplify' (Operator ("+", (+)) x y)             = Operator ("+", (+)) (simplify' x) (simplify' y)
-simplify' (Operator ("-", (-)) (Constant 0) y)  = Operator ("*", (*)) (Constant (-1)) (simplify' y)
-simplify' (Operator ("-", (-)) x (Constant 0))  = simplify' x
-simplify' (Operator ("-", (-)) x y)             = Operator ("-", (-)) (simplify' x) (simplify' y)
-
 deriveBy :: String -> Node -> Node
-deriveBy _ (Constant _) = Constant 0
-deriveBy var (Variable x)
+deriveBy var x = simplify $ deriveBy' var x
+
+deriveBy' :: String -> Node -> Node
+deriveBy' _ (Constant _) = Constant 0
+deriveBy' var (Variable x)
   | x == var = Constant 1
   | otherwise = Constant 0
-deriveBy var (Function f x)
-  | f == "sin" = Operator ("*", (*)) (Function "cos" x) (deriveBy var x)
-  | f == "cos" = Operator ("*", (*)) (Operator ("*", (*)) (Constant (-1)) (Function "cos" x)) (deriveBy var x)
-  | f == "exp" = Operator ("*", (*)) (Function f x) (deriveBy var x)
-  | f == "log" = Operator ("*", (*)) (Operator ("/", (/)) (Constant 1) x) (deriveBy var x)
+deriveBy' var (Function f x)
+  | f == "sin" = Operator ("*", (*)) (Function "cos" x) (deriveBy' var x)
+  | f == "cos" = Operator ("*", (*)) (Operator ("*", (*)) (Constant (-1)) (Function "cos" x)) (deriveBy' var x)
+  | f == "exp" = Operator ("*", (*)) (Function f x) (deriveBy' var x)
+  | f == "log" = Operator ("*", (*)) (Operator ("/", (/)) (Constant 1) x) (deriveBy' var x)
   | otherwise = error "Unsupported function"
-deriveBy var (Operator ("*", _) x y) =
-  Operator ("+", (+)) (Operator ("*", (*)) (deriveBy var x) (y)) (Operator ("*", (*)) (x) (deriveBy var y))
-deriveBy var (Operator ("/", _) x y) =
-  Operator ("/", (/)) (Operator ("-", (-)) (Operator ("*", (*)) (deriveBy var x) y) (Operator ("*", (*)) x (deriveBy var y))) (Operator ("^", (**)) y (Constant 2))
-deriveBy var (Operator ("+", _) x y) = Operator ("+", (+)) (deriveBy var x) (deriveBy var y)
-deriveBy var (Operator ("-", _) x y) = Operator ("-", (-)) (deriveBy var x) (deriveBy var y)
-deriveBy var (Operator ("^", _) x (Constant y)) =
-  Operator ("*", (*)) (Operator ("*", (*)) (Constant y) (Operator ("^", (**)) (x) (Constant (y-1)))) (deriveBy var x)
+deriveBy' var (Operator ("*", _) x y) =
+  Operator ("+", (+)) (Operator ("*", (*)) (deriveBy' var x) (y)) (Operator ("*", (*)) (x) (deriveBy' var y))
+deriveBy' var (Operator ("/", _) x y) =
+  Operator ("/", (/)) (Operator ("-", (-)) (Operator ("*", (*)) (deriveBy' var x) y) (Operator ("*", (*)) x (deriveBy' var y))) (Operator ("^", (**)) y (Constant 2))
+deriveBy' var (Operator ("+", _) x y) = Operator ("+", (+)) (deriveBy' var x) (deriveBy' var y)
+deriveBy' var (Operator ("-", _) x y) = Operator ("-", (-)) (deriveBy' var x) (deriveBy' var y)
+deriveBy' var (Operator ("^", _) x (Constant y)) =
+  Operator ("*", (*)) (Operator ("*", (*)) (Constant y) (Operator ("^", (**)) (x) (Constant (y-1)))) (deriveBy' var x)
 
 evaluate :: Node -> Double
 evaluate (Variable _)      = error "The tree still contains variables."
@@ -95,11 +33,15 @@ evaluate (Operator op x y) = (snd op) (evaluate x) (evaluate y)
 evaluate (Function fun x)  = f (evaluate x)
   where f = snd $ head $ filter ((==fun) . fst) functions
 
+
 substitute :: String -> Double -> Node -> Node
-substitute _ _ (Constant x)          = Constant x
-substitute var val (Function _ x)    = substitute var val x
-substitute var val (Operator op x y) = Operator op (substitute var val x) (substitute var val y)
-substitute var val (Variable x)
+substitute var val x = simplify $ substitute' var val x
+
+substitute' :: String -> Double -> Node -> Node
+substitute' _ _ (Constant x)          = Constant x
+substitute' var val (Function f x)    = Function f (substitute' var val x)
+substitute' var val (Operator op x y) = Operator op (substitute' var val x) (substitute' var val y)
+substitute' var val (Variable x)
   | var == x                         = Constant val
   | otherwise                        = Variable x
 
@@ -176,3 +118,68 @@ associativity :: String -> String
 associativity op = case op of
   "^" -> "Right"
   _   -> "Left"
+
+simplify :: Node -> Node
+simplify t = if t == tt then t else simplify tt
+  where tt = simplify' t
+
+simplify' :: Node -> Node
+simplify' (Constant x)                          = Constant x
+simplify' (Variable x)                          = Variable x
+simplify' (Function f x)                        = Function f (simplify' x)
+simplify' (Operator ("*", (*)) (Constant 0) x)  = Constant 0
+simplify' (Operator ("*", (*)) x (Constant 0))  = Constant 0
+simplify' (Operator ("*", (*)) x (Constant 1))  = simplify' x
+simplify' (Operator ("*", (*)) (Constant 1) x)  = simplify' x
+simplify' (Operator ("*", (*)) x y)             = Operator ("*", (*)) (simplify' x) (simplify' y)
+simplify' (Operator ("^", (**)) x (Constant 1)) = simplify' x
+simplify' (Operator ("^", (**)) x (Constant 0)) = Constant 1
+simplify' (Operator ("^", (**)) (Constant 1) x) = Constant 1
+simplify' (Operator ("^", (**)) x y)            = Operator ("^", (**)) (simplify' x) (simplify' y)
+simplify' (Operator ("/", (/)) (Constant 0) y)  = Constant 0
+simplify' (Operator ("/", (/)) x (Constant 1))  = simplify' x
+simplify' (Operator ("/", (/)) x y)             = Operator ("/", (/)) (simplify' x) (simplify' y)
+simplify' (Operator ("+", (+)) (Constant 0) y)  = simplify' y
+simplify' (Operator ("+", (+)) x (Constant 0))  = simplify' x
+simplify' (Operator ("+", (+)) x y)             = Operator ("+", (+)) (simplify' x) (simplify' y)
+simplify' (Operator ("-", (-)) (Constant 0) y)  = Operator ("*", (*)) (Constant (-1)) (simplify' y)
+simplify' (Operator ("-", (-)) x (Constant 0))  = simplify' x
+simplify' (Operator ("-", (-)) x y)             = Operator ("-", (-)) (simplify' x) (simplify' y)
+
+instance Eq Node where
+  (Constant x) == (Constant y)                     = x == y
+  (Variable x) == (Variable y)                     = x == y
+  (Function f x) == (Function g y)                 = f == g && x == y
+  (Operator (o1, _) a b) == (Operator (o2, _) c d) = o1 == o2 && a == c && b == d
+  _ == _                                           = False
+
+instance Show Node where
+  show (Constant x)                                            = show x
+  show (Variable x)                                            = x
+  show (Function f x)                                          = f ++ " ( " ++ show x ++ " ) "
+  show (Operator ("*", _) x@(Operator (op1, _) _ _) y@(Operator (op2, _) _ _))
+    | (op1 == "+" || op1 == "-") && (op2 == "+" || op2 == "-") = " ( " ++ show x ++ " ) * ( " ++ show y ++ " ) "
+    | op1 == "+" || op1 == "-"                                 = " ( " ++ show x ++ " ) * " ++ show y
+    | op2 == "+" || op2 == "-"                                 = show x ++ " * ( " ++ show y ++ " ) "
+    | otherwise                                                = show x ++ " * " ++ show y
+  show (Operator ("*", _) x y@(Operator (op, _) _ _))
+    | op == "+" || op == "-"                                   = show x ++ " * ( " ++ show y ++ " ) "
+    | otherwise                                                = show x ++ " * " ++ show y
+  show (Operator ("*", _) x@(Operator (op, _) _ _) y)
+    | op == "+" || op == "-"                                   = "( " ++ show x ++ " ) * " ++ show y
+    | otherwise                                                = show x ++ " * " ++ show y
+  show (Operator ("/", _) x@(Operator (op1, _) _ _) y@(Operator (op2, _) _ _))
+    | (op1 == "+" || op1 == "-") && (op2 == "+" || op2 == "-") = " ( " ++ show x ++ " ) / ( " ++ show y ++ " ) "
+    | op1 == "+" || op1 == "-"                                 = " ( " ++ show x ++ " ) / " ++ show y
+    | op2 == "+" || op2 == "-"                                 = show x ++ " / ( " ++ show y ++ " ) "
+    | otherwise                                                = show x ++ " / " ++ show y
+  show (Operator ("/", _) x y@(Operator (op, _) _ _))
+    | op == "+" || op == "-"                                   = show x ++ " / ( " ++ show y ++ " ) "
+    | otherwise                                                = show x ++ " / " ++ show y
+  show (Operator ("/", _) x@(Operator (op, _) _ _) y)
+    | op == "+" || op == "-"                                   = "( " ++ show x ++ " ) / " ++ show y
+    | otherwise                                                = show x ++ " / " ++ show y
+  show (Operator ("^", _) x@(Operator (op, _) _ _) y)
+    | op == "+" || op == "-"                                   = " ( " ++ show x ++ " ) ^ " ++ show y
+    | otherwise                                                = show x ++ " " ++ op ++ " " ++ show y
+  show (Operator op x y)                                       = show x ++ " " ++ fst op ++ " " ++ show y
